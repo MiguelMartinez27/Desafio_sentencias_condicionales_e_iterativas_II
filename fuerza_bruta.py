711f041333355a8d949fb07063e0b1f256d2c0f2
MiguelMartinez27 <miguel.martinez.fue@gmail.com>
2024-07-31 19:01:46 -0400

    Update fuerza_bruta.py

--- fuerza_bruta.py
+++ fuerza_bruta.py
@@ -1,20 +1,20 @@
-from string import ascii_lowercase
 import getpass
+from string import ascii_lowercase
 
-def fuerza_bruta(password):
-    intentos = 0
-    encontrado = ''
+#Solicitar la contrasena de manera oculta
+password = getpass.getpass("Ingrese la contraseña: ").strip().lower()
 
-    for letra in password:
-        for posible in ascii_lowercase:
-            intentos += 1
-            if posible == letra:
-                encontrado += posible
-                break
+intentos = 0
 
-    return intentos
+guessed_password = ""
 
+#Realiza la busqueda de fuerza bruta
+for char in password: 
+    for letter in ascii_lowercase:
+        intentos += 1
+        if char == letter:
+            guessed_password += letter
+            break
 
-password = getpass.getpass("Ingrese la contraseña: ").lower()  # Oculta la entrada y convierte a minúsculas
-intentos = fuerza_bruta(password)
-print(f"La contraseña fue forzada en {intentos} intentos")
+print(f"La contraseña fue forzada en {intentos} intentos.")
+print(f"La contraseña adivinada es: {guessed_password}")
